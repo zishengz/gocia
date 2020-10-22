34a26ddb6cf724c562283749fb7d277be6f594e0
Zisheng <superqianzhang@qq.com>
2020-10-22 01:45:35 -0500

    enabled xtb: periodic GFN0 calculator

---
+++ tests/geneAlgo_xtbCalc/opt-batch.py
@@ -0,0 +1,25 @@
+import sys
+from ase.db import connect
+from gocia.utils.dbio import get_traj
+
+def opt_xtb_lbfgs(atoms):
+    import xtb
+    from xtb import GFN0
+    from ase.optimize.lbfgs import LBFGS
+    tmpAtoms = atoms.copy()
+    tmpAtoms.calc = GFN0()
+    geomOpt = LBFGS(tmpAtoms, maxstep=0.1,trajectory=None,logfile=None)
+    geomOpt.run(fmax=0.03, steps=1000)
+    return tmpAtoms
+
+
+inpName = sys.argv[1]
+oldDB = connect(inpName)
+traj = get_traj(oldDB.select())
+
+newDB = connect('out-'+inpName, append=False)
+for i in range(len(traj)):
+    print('Optimizing\t%i/%i'%(i+1, len(traj)))
+    opt = opt_emt_lbfgs(traj[i])
+    newDB.write(opt, eV=opt.get_potential_energy(), mag=0)
+
