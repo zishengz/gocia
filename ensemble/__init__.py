821ded5008ac0eb087bd624d8f89fbb9cf14a56c
Zisheng <superqianzhang@qq.com>
2020-11-06 12:08:30 -0600

    gc filtering update

--- ensemble/__init__.py
+++ ensemble/__init__.py
@@ -121,3 +121,69 @@ def clusterIsomerAbs(
                 mag = magUniq[eneUniq.index(sorted(eneUniq)[i])],
                 counts = countUniq[eneUniq.index(sorted(eneUniq)[i])],
             )
+
+def clusterIsomerAbs_GC(
+    trajAtoms, # Must containg energy information
+    eneToler = 0.05,
+    geomToler1=5e-4,
+    geomToler2=0.25,
+    outName='sort'
+    ):
+    import gocia.ensemble.comparator as comp
+
+    # Handle the bad or other-format outputs
+    if 'mag' in trajAtoms[0].info.keys():
+        magArray = [a.info['mag'] for a in trajAtoms]
+    else:
+        magArray = [0]*len(trajAtoms)
+    if 'grandPot' in trajAtoms[0].info.keys():
+        eneArray = [a.info['grandPot'] for a in trajAtoms]
+    else:
+        eneArray = [a.get_potential_energy() for a in trajAtoms]
+
+    print(' * Detecting unique isomers...')
+    nStates = len(trajAtoms)
+    isomerLabel = [-1]*nStates
+    currIso, currInd = [], 0
+    while -1 in isomerLabel:
+        if currInd >= nStates:
+            break
+        if isomerLabel[currInd] != -1:
+            currInd += 1
+            continue
+        print('   |-Isomer %i'%len(list(set(isomerLabel))), end='\t:')
+        for i in range(nStates):
+            if isomerLabel[i] != -1:
+                continue
+            # if simMat[currInd][i] == 1:
+            #     isomerLabel[i] = currInd
+            if abs(eneArray[i] - eneArray[currInd]) < eneToler and\
+                comp.srtDist_similar_zz(trajAtoms[i],trajAtoms[currInd],\
+                    delta_rel=geomToler1, d_max=geomToler2):
+                isomerLabel[i] = currInd
+                print(i, end=',')
+        currInd += 1
+        print('')
+
+    isoUniq = set(isomerLabel)
+    eneUniq = [eneArray[i] for i in isoUniq]
+    magUniq = [magArray[i] for i in isoUniq]
+    countUniq = [isomerLabel.count(i) for i in isoUniq]
+#    print(countUniq)
+    print('-'*76)
+    print('   %i\tisomers found from %s\tsamples. Oversampling = %s'%\
+        (len(isoUniq), nStates, '{:.2%}'.format(nStates/len(isoUniq)-1)))
+    sortedTraj = []
+    for v in sorted(eneUniq):
+        sortedTraj.append(trajAtoms[list(eneArray).index(v)])
+
+    with connect('%s.db'%outName, append=False) as srtDB:
+        for i in range(len(sortedTraj)):
+            srtDB.write(
+                sortedTraj[i],
+                done=1,
+                eV = sorted(eneUniq)[i],
+                eV2GM = sorted(eneUniq)[i] - min(eneUniq),
+                mag = magUniq[eneUniq.index(sorted(eneUniq)[i])],
+                counts = countUniq[eneUniq.index(sorted(eneUniq)[i])],
+            )
