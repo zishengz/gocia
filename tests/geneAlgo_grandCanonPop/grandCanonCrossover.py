5bd44fb31a5f63e70309f3b10ef73ea0e27e8c37
Zisheng <superqianzhang@qq.com>
2020-11-09 17:48:52 -0600

    bugfix that fail to do leach mutation

--- tests/geneAlgo_grandCanonPop/grandCanonCrossover.py
+++ tests/geneAlgo_grandCanonPop/grandCanonCrossover.py
@@ -1,5 +1,5 @@
 from gocia.interface import Interface
-import sys
+import sys, os
 from gocia.ga.crossover import crossover_snsSurf_2d_GC
 
 subs = sys.argv[1]
@@ -11,7 +11,12 @@ surf1 = Interface(allAtoms=surf1, subAtoms=subs)
 surf2 = Interface(allAtoms=surf2, subAtoms=subs)
 
 surf1.print()
+surf1.draw('CPK')
+os.system('mv Ga47N51-cpk.png mom.png')
+
 surf2.print()
+surf2.draw('CPK')
+os.system('mv Ga47N51-cpk.png dad.png')
 
 #print(surf1.get_fixBufPos())
 #print(surf1.get_fixBufAtoms())
@@ -19,3 +24,21 @@ surf2.print()
 nova = crossover_snsSurf_2d_GC(surf1, surf2)
 nova.print()
 nova.write('nova.vasp')
+nova.draw('CPK')
+os.system('mv Ga47N51-cpk.png kid.png')
+tmp = nova.copy()
+
+nova = tmp.copy()
+nova.rattleMut()
+nova.draw('CPK')
+os.system('mv Ga47N51-cpk.png kid-rattle.png')
+
+nova = tmp.copy()
+nova.growMut(['N'])
+nova.draw('CPK')
+os.system('mv Ga47N51-cpk.png kid-grow.png')
+
+nova = tmp.copy()
+nova.leachMut(['N'])
+nova.draw('CPK')
+os.system('mv Ga47N51-cpk.png kid-leach.png')
