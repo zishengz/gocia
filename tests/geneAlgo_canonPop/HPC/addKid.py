62fa4be859445c6f5be92efddf732b14952d2672
Zisheng Zhang <superqianzhang@qq.com>
2021-08-24 17:53:31 -0500

    test files clean up

--- tests/geneAlgo_canonPop/HPC/addKid.py
+++
@@ -1,6 +0,0 @@
-from gocia.ga.popCanon import PopulationCanonical
-
-pop = PopulationCanonical(gadb='../B4O6H1.db', substrate='../substrate.vasp', zLim=[7.5, 10.5])
-
-pop.add_vaspResult()
-pop.natural_selection()
--- tests/lammps_reaxff/all2reaxPos.py
+++
@@ -1,8 +0,0 @@
-from ase.io import read, write
-import sys
-
-fn_inp = sys.argv[1]
-
-s = read(fn_inp)
-
-write('%s.lammps-data'%(fn_inp.split('.')[0]), s, atom_style='charge')
