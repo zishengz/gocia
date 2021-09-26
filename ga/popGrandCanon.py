5e65b1f2600a205eb2ae63c785b4c750f84e3200
zishengz <zisheng@chem.ucla.edu>
2021-09-26 15:16:59 -0500

    now use vasp atoms reader

--- ga/popGrandCanon.py
+++ ga/popGrandCanon.py
@@ -3,28 +3,29 @@ import numpy as np
 from ase.db import connect
 from gocia.interface import Interface
 from gocia.ga import (
-                    get_eneFactor,
-                    get_matedFactor,
-                    get_matedFactor2,
-                    get_matedFactor3
-                    )
+    get_eneFactor,
+    get_matedFactor,
+    get_matedFactor2,
+    get_matedFactor3
+)
 from gocia.ga.crossover import crossover_snsSurf_2d_GC
 from gocia.ensemble.comparator import srtDist_similar_zz
 from ase.io import read, write
 
+
 class PopulationGrandCanonical:
     def __init__(
         self,
-        substrate = None,
-        gadb = None,
-        popSize = 20,
-        zLim = None,
-        subsPot = 0,
-        chemPotDict = None,
-        compParam = None,
-        matingMethod = None,
-        convergeCrit = None,
-        ):
+        substrate=None,
+        gadb=None,
+        popSize=20,
+        zLim=None,
+        subsPot=0,
+        chemPotDict=None,
+        compParam=None,
+        matingMethod=None,
+        convergeCrit=None,
+    ):
 
         if gadb is not None:
             self.gadb = connect(gadb)
@@ -71,8 +72,8 @@ class PopulationGrandCanonical:
             return len(self) - gmid > self.convergeCrit
 
     def calc_grandPot(self, atoms, dftene):
-#        myRow = self.gadb.get(id=myID)
-#        myPot = myRow['eV']
+        #        myRow = self.gadb.get(id=myID)
+        #        myPot = myRow['eV']
         myPot = dftene - self.subsPot
         adsSymbol = Interface(atoms, self.substrate).\
             get_adsAtoms().get_chemical_symbols()
@@ -84,13 +85,12 @@ class PopulationGrandCanonical:
     def initializeDB(self):
         for i in range(len(self.gadb)):
             r = self.gadb.get(id=i+1)
-            self.gadb.update(i+1, mated=0, alive=1,\
-                grandPot=self.calc_grandPot(r.toatoms(), r.eV), label='0 0 init')
+            self.gadb.update(i+1, mated=0, alive=1,
+                             grandPot=self.calc_grandPot(r.toatoms(), r.eV), label='0 0 init')
         self.natural_selection()
-        gm=self.get_GMrow()
+        gm = self.get_GMrow()
         with open('gmid', 'w') as f:
             f.write(str(self.get_GMrow().id))
-
 
     def get_ID(self, condString):
         tmp = []
@@ -128,15 +128,15 @@ class PopulationGrandCanonical:
         parents = np.random.choice(aliveList, size=2, replace=False, p=weights)
         parents = [int(parents[0]), int(parents[1])]
         return parents
-    
+
     def natural_selection(self):
         aliveList = self.get_ID('alive=1')
         if len(aliveList) > self.popSize:
             fitnessList = self.get_fitness(aliveList)
-            aliveList = [x for _,x in sorted(zip(fitnessList, aliveList))]
+            aliveList = [x for _, x in sorted(zip(fitnessList, aliveList))]
             deadList = aliveList[:-self.popSize]
             for d in deadList:
-                print('REST IN PEACE, %i!'%d)
+                print('REST IN PEACE, %i!' % d)
                 self.gadb.update(d, alive=0)
 
     def is_uniqueInPop(self, atoms, grandPot):
@@ -155,7 +155,7 @@ class PopulationGrandCanonical:
                     break
         return isUnique
 
-    def gen_offspring(self, mutRate=0.3, rattleOn=True, growOn=True, leachOn=True, permuteOn = True, transOn = True, transVec=[[-2,2],[-2,2]]):
+    def gen_offspring(self, mutRate=0.3, rattleOn=True, growOn=True, leachOn=True, permuteOn=True, transOn=True, transVec=[[-2, 2], [-2, 2]]):
         kid, parent = None, None
         mater, pater = 0, 0
         while kid is None:
@@ -166,15 +166,15 @@ class PopulationGrandCanonical:
             surf2 = Interface(a2, self.substrate, zLim=self.zLim)
             kid = crossover_snsSurf_2d_GC(surf1, surf2, tolerance=0.75)
             parent = surf1.copy()
-        print('PARENTS: %i and %i'%(mater, pater))
+        print('PARENTS: %i and %i' % (mater, pater))
         myMutate = ''
         if srtDist_similar_zz(a1, a2)\
-            or srtDist_similar_zz(a1, kid.get_allAtoms())\
-            or srtDist_similar_zz(a2, kid.get_allAtoms()):
+                or srtDist_similar_zz(a1, kid.get_allAtoms())\
+                or srtDist_similar_zz(a2, kid.get_allAtoms()):
             print(' |- TOO SIMILAR!')
             mutRate = 1
         if np.random.rand() < mutRate:
-            mutType = np.random.choice([0,1,2,3,4], size=1)[0]
+            mutType = np.random.choice([0, 1, 2, 3, 4], size=1)[0]
             if mutType == 0 and rattleOn:
                 myMutate = 'rattle'
                 kid.rattleMut()
@@ -196,12 +196,12 @@ class PopulationGrandCanonical:
             kid = parent.copy()
             kid.rattleMut()
             kid.growMut([l for l in self.chemPotDict])
-        open('label', 'w').write('%i %i %s'%(mater, pater, myMutate))
+        open('label', 'w').write('%i %i %s' % (mater, pater, myMutate))
         self.gadb.update(mater, mated=self.gadb.get(id=mater).mated+1)
         self.gadb.update(pater, mated=self.gadb.get(id=pater).mated+1)
         return kid
 
-    def gen_offspring_box(self, mutRate=0.3, xyzLims=[], bondRejList = None, constrainTop=False, rattleOn=True, growOn=True, leachOn=True, permuteOn = True, transOn = True, transVec=[[-2,2],[-2,2]]):
+    def gen_offspring_box(self, mutRate=0.3, xyzLims=[], bondRejList=None, constrainTop=False, rattleOn=True, growOn=True, leachOn=True, permuteOn=True, transOn=True, transVec=[[-2, 2], [-2, 2]]):
         kid, parent = None, None
         mater, pater = 0, 0
         while kid is None:
@@ -212,21 +212,22 @@ class PopulationGrandCanonical:
             surf2 = Interface(a2, self.substrate, zLim=self.zLim)
             kid = crossover_snsSurf_2d_GC(surf1, surf2, tolerance=0.75)
             parent = surf1.copy()
-        print('PARENTS: %i and %i'%(mater, pater))
+        print('PARENTS: %i and %i' % (mater, pater))
         myMutate = ''
         if srtDist_similar_zz(a1, a2)\
-            or srtDist_similar_zz(a1, kid.get_allAtoms())\
-            or srtDist_similar_zz(a2, kid.get_allAtoms()):
+                or srtDist_similar_zz(a1, kid.get_allAtoms())\
+                or srtDist_similar_zz(a2, kid.get_allAtoms()):
             print(' |- TOO SIMILAR!')
             mutRate = 1
         if np.random.rand() < mutRate:
-            mutType = np.random.choice([0,1,2,3,4], size=1)[0]
+            mutType = np.random.choice([0, 1, 2, 3, 4], size=1)[0]
             if mutType == 0 and rattleOn:
                 myMutate = 'rattle'
                 kid.rattleMut()
             if mutType == 1 and growOn:
                 myMutate = 'grow'
-                kid.growMut_box([l for l in self.chemPotDict], xyzLims=xyzLims, bondRejList=bondRejList, constrainTop=constrainTop)
+                kid.growMut_box([l for l in self.chemPotDict], xyzLims=xyzLims,
+                                bondRejList=bondRejList, constrainTop=constrainTop)
             if mutType == 2 and leachOn:
                 myMutate = 'leach'
                 kid.leachMut([l for l in self.chemPotDict])
@@ -242,7 +243,7 @@ class PopulationGrandCanonical:
             kid = parent.copy()
             kid.rattleMut()
             kid.growMut([l for l in self.chemPotDict])
-        open('label', 'w').write('%i %i %s'%(mater, pater, myMutate))
+        open('label', 'w').write('%i %i %s' % (mater, pater, myMutate))
         self.gadb.update(mater, mated=self.gadb.get(id=mater).mated+1)
         self.gadb.update(pater, mated=self.gadb.get(id=pater).mated+1)
         return kid
@@ -251,18 +252,17 @@ class PopulationGrandCanonical:
         import os
         cwdFiles = os.listdir(vaspdir)
         if 'OSZICAR' in cwdFiles\
-            and 'BADSTRUCTURE' not in cwdFiles\
-            and 'ERROR' not in open('%s/OSZICAR'%vaspdir).read():
-            info = [l for l in open('%s/OSZICAR'%vaspdir).readlines() if 'F' in l]
-            if len(info) > 0:
-                info = info[-1].split()
-                mag = eval(info[-1])
-                ene_eV = eval(info[4])
-                s = read('%s/CONTCAR'%vaspdir)
-                s.wrap()
+                and 'BADSTRUCTURE' not in cwdFiles\
+                and 'ERROR' not in open('%s/OSZICAR' % vaspdir).read():
+            if 'E0' in open(vaspdir+'/OSZICAR', 'r').readlines()[-1]:
+                s = read('%s/OUTCAR' % vaspdir, index='-1')
+                info = os.getcwd().split('/')[-1]
+                mag = s.get_magnetic_moment()
+                ene_eV = s.get_potential_energy()
                 grndPot = self.calc_grandPot(s, ene_eV)
                 myLabel = open('label', 'r').read()
-                print('\nA CHILD IS BORN with G = %.3f eV\t[%s]'%(grndPot, myLabel))
+                print('\nA CHILD IS BORN with G = %.3f eV\t[%s]' % (
+                    grndPot, myLabel))
                 if self.is_uniqueInPop(s, grndPot):
                     if grndPot < self.get_GMrow()['grandPot']:
                         print(' |- it is the new GM!')
@@ -270,30 +270,29 @@ class PopulationGrandCanonical:
                             f.write(str(len(self)))
                     self.gadb.write(
                         s,
-                        mag     = mag,
-                        eV      = ene_eV,
-                        grandPot= grndPot,
-                        mated   = 0,
-                        done    = 1,
-                        alive   = 1,
-                        label = myLabel
+                        name=info,
+                        mag=mag,
+                        eV=ene_eV,
+                        grandPot=grndPot,
+                        mated=0,
+                        done=1,
+                        alive=1,
+                        label=myLabel
                     )
                 else:
                     print(' |- it is a duplicate!')
                     self.gadb.write(
                         s,
-                        mag     = mag,
-                        eV      = ene_eV,
-                        grandPot= grndPot,
-                        mated   = 0,
-                        done    = 1,
-                        alive   = 0,
-                        label = myLabel
+                        name=info,
+                        mag=mag,
+                        eV=ene_eV,
+                        grandPot=grndPot,
+                        mated=0,
+                        done=1,
+                        alive=0,
+                        label=myLabel
                     )
 
 # TODO convergence: TEST
 # TODO XTB interface
 # TODO CP2K interface
-
-
-    
--- utils/dbio.py
+++ utils/dbio.py
@@ -3,6 +3,7 @@ import numpy as np
 from ase.db import connect
 from ase.io import read, write
 
+
 def get_keyVal(dbRows, keyVal):
     '''
     Returns 1-D list
@@ -12,6 +13,7 @@ def get_keyVal(dbRows, keyVal):
         tmp.append(r[keyVal])
     return tmp
 
+
 def get_traj(dbRows):
     tmp = []
     for r in dbRows:
@@ -20,81 +22,91 @@ def get_traj(dbRows):
         tmp.append(a)
     return tmp
 
+
 def get_projName():
     return os.getcwd().split('/')[-1]
+
 
 def calypso2db():
     print(' --- Collecting CALYPSO results --- ')
     os.system('cd results;cak.py -n 9999 --vasp')
 #    eneData = [eval(r.split()[-1]) for r in open('results/Analysis_Output.dat').readlines()[1:]]
-    eneData = [r.split()[-1] for r in open('results/Analysis_Output.dat').readlines()[1:]]
-    eneData = [0 if r=='NULL' else eval(r) for r in eneData]
-    with connect('calypso-%s.db'%get_projName(), append=False) as myDb:
+    eneData = [r.split()[-1]
+               for r in open('results/Analysis_Output.dat').readlines()[1:]]
+    eneData = [0 if r == 'NULL' else eval(r) for r in eneData]
+    with connect('calypso-%s.db' % get_projName(), append=False) as myDb:
         for i in range(len(eneData)):
-            s = read('results/dir_origin/OCell_%i.vasp'%(i+1))
+            s = read('results/dir_origin/OCell_%i.vasp' % (i+1))
             s.wrap()
             myDb.write(
                 s,
-                eV = eneData[i],
-                done = 1,
+                eV=eneData[i],
+                done=1,
             )
-    print(' %i candidates writen to calypso-%s.db'%(len(eneData), get_projName()))
-            
+    print(' %i candidates writen to calypso-%s.db' %
+          (len(eneData), get_projName()))
+
+
 def vasp2db(nameKey='', excludeBAD=False):
     print(' --- Collecting VASP results ---')
-    vdirs = [d.split('/')[0] for d in os.popen('ls *%s*/OSZICAR'%nameKey).readlines()]
+    vdirs = [d.split('/')[0]
+             for d in os.popen('ls *%s*/OSZICAR' % nameKey).readlines()]
     count_fin = 0
-    with connect('vasp-%s.db'%get_projName(), append=False) as myDb:
+    with connect('vasp-%s.db' % get_projName(), append=False) as myDb:
         for d in vdirs:
+            print('%s' % d, end='')
             if excludeBAD:
                 if 'BADSTRUCTURE' in os.listdir(d):
+                    print('-X', end='\t')
                     continue
-            print('%s'%d, end='\t')
-#            info = open(d+'/OSZICAR', 'r').readlines()[-1].split()
-            info = [l for l in open(d+'/OSZICAR', 'r').readlines() if 'F' in l ][-1].split()
-            mag = eval(info[-1])
-            ene_eV = eval(info[4])
-            s = read(d+'/CONTCAR')
-            s.wrap()
+            if 'E0' not in open(d+'/OSZICAR', 'r').readlines()[-1]:
+                print('-X', end='\t')
+                continue
+            s = read(d+'/OUTCAR', index='-1')
             myDb.write(
-				s,
-				eV=ene_eV,
-				mag=mag,
+                s,
+                eV=s.get_potential_energy(),
+                mag=s.get_magnetic_moment(),
                 done=1,
-                )
+            )
+            print('-O', end='\t')
             count_fin += 1
-    print('\n %i candidates writen to vasp-%s.db'%(count_fin, get_projName()))
+    print('\n %i candidates writen to vasp-%s.db' %
+          (count_fin, get_projName()))
+
 
 def lmp2db(nameKey='', excludeBAD=False):
     import gocia.utils.lammps as lmp
     print(' --- Collecting LAMMPS results ---')
-    vdirs = [d.split('/')[0] for d in os.popen('ls *%s*/lmp.out'%nameKey).readlines()]
+    vdirs = [d.split('/')[0]
+             for d in os.popen('ls *%s*/lmp.out' % nameKey).readlines()]
     count_fin = 0
-    with connect('lmp-%s.db'%get_projName(), append=False) as myDb:
+    with connect('lmp-%s.db' % get_projName(), append=False) as myDb:
         for d in vdirs:
             if excludeBAD:
                 if 'BADSTRUCTURE' in os.listdir(d):
                     continue
             if 'Final' not in open(d+'/lmp.out').read():
                 continue
-            print('%s'%d, end='\t')
+            print('%s' % d, end='\t')
             info = ''
             mag = 0
             ene_eV = lmp.get_ene(d+'/lmp.out')
             s = lmp.get_last_frame(d+'/traj.xyz', d+'/inp.vasp')
             s.wrap()
             myDb.write(
-				s,
-				eV=ene_eV,
-				mag=mag,
+                s,
+                eV=ene_eV,
+                mag=mag,
                 done=1,
-                )
+            )
             count_fin += 1
-    print('\n %i candidates writen to lmp-%s.db'%(count_fin, get_projName()))
+    print('\n %i candidates writen to lmp-%s.db' % (count_fin, get_projName()))
+
 
 def db2vasp(dbName):
     traj = read(dbName, index=':')
     for i in range(len(traj)):
-        curName = 's%s.vasp'%(str(i).zfill(6))
+        curName = 's%s.vasp' % (str(i).zfill(6))
         write(curName, traj[i], vasp5=True)
-        print(' > %s written!'%curName)
+        print(' > %s written!' % curName)
