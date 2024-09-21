2836dbc2de820e541d3d644f94d13a630211f3ae
zishengz <zisheng@chem.ucla.edu>
2024-09-21 00:36:05 -0700

    ase interface bugfix: backup and calculator info

--- gocia/utils/ase.py
+++ gocia/utils/ase.py
@@ -48,13 +48,17 @@ def geomopt_multi(atoms, list_calc, optimizer='LBFGS', list_fmax=None, label=Non
         for i_step in range(len(list_calc)):
             print(f'STEP {i_step+1}')
             atoms_opt = geomopt_simple(atoms_opt, list_calc[i_step], optimizer, list_fmax[i_step], label=label)
-            os.rename('opt.traj', f'opt{i_step+1}.traj')
+            if label is None:
+                label = '.'
+            os.rename(f'{label}/opt.traj', f'{label}/opt{i_step+1}.traj')
     else:
         for i_step in range(len(list_calc)):
             print(f'STEP {i_step+1}')
             atoms_opt = geomopt_simple(atoms_opt, list_calc[i_step], optimizer, label=label)
             if fn_bkup is not None:
-                os.rename(fn_bkup, f'opt{i_step+1}__{fn_bkup}')
+                if label is None:
+                    label = '.'
+                os.rename(f'{label}/{fn_bkup}', f'{label}/opt{i_step+1}__{fn_bkup}')
     return atoms_opt
 
 
@@ -68,146 +72,150 @@ def geomopt_iterate(atoms, my_calc, optimizer='LBFGS', fmax=None, label=None, ch
     counter = 0
     my_atoms = atoms.copy()
     while continueRunning:
-            print(f'Optimization cycle: {counter + 1}')
-            if optimizer is not None and fmax is not None:
-                if type(my_calc) is list and type(fmax) is list:
-                    opt_atoms = geomopt_multi(my_atoms, my_calc, optimizer, fmax, label=label, fn_bkup=fn_bkup)
-                elif type(my_calc) is not list and type(fmax) is not list:
-                    opt_atoms = geomopt_simple(my_atoms, my_calc, optimizer, fmax, label=label, fn_bkup=fn_bkup)
-                else:
-                    print('Inconsistent #fmax and #calc\\ -- they should either be both lists ot both single objects/variables.')
-                for fb in [f for f in os.listdir('.') if f[:3] == 'opt' and f[5:]=='.traj']:
-                    os.rename(fb, f'cyc{str(counter).zfill(2)}__{fb}')
+        print(f'Optimization cycle: {counter + 1}')
+        if optimizer is not None and fmax is not None:
+            if type(my_calc) is list and type(fmax) is list:
+                opt_atoms = geomopt_multi(my_atoms, my_calc, optimizer, fmax, label=label, fn_bkup=fn_bkup)
+            elif type(my_calc) is not list and type(fmax) is not list:
+                opt_atoms = geomopt_simple(my_atoms, my_calc, optimizer, fmax, label=label, fn_bkup=fn_bkup)
             else:
-                if type(my_calc) is list:
-                    opt_atoms = geomopt_multi(my_atoms, my_calc, optimizer, label=label, fn_bkup=fn_bkup)
-                elif type(my_calc) is not list:
-                    opt_atoms = geomopt_simple(my_atoms, my_calc, optimizer, label=label, fn_bkup=fn_bkup)
-                # back up files
-                if fn_bkup is not None:
-                    for fb in [f for f in os.listdir('.') if f[:3] == 'opt' and fn_bkup in f]:
-                        os.rename(fb, f'cyc{str(counter).zfill(2)}__{fb}')
-                
-            counter += 1
+                print('Inconsistent #fmax and #calc\\ -- they should either be both lists ot both single objects/variables.')
 
-            # REMOVE BROKEN FRAGMENTS
-            if has_fragList:
-                my_fragList = read_frag(fn=fn_frag)
-                struct = opt_atoms.copy()
-                my_fragAtoms = [struct[f] for f in my_fragList]
-                # Check connectivity
+            for fb in [f for f in os.listdir('.') if f[:3] == 'opt' and f[5:]=='.traj']:
+                os.rename(fb, f'cyc{str(counter).zfill(2)}__{fb}')
+        else:
+            if type(my_calc) is list:
+                opt_atoms = geomopt_multi(my_atoms, my_calc, optimizer, label=label, fn_bkup=fn_bkup)
+            elif type(my_calc) is not list:
+                opt_atoms = geomopt_simple(my_atoms, my_calc, optimizer, label=label, fn_bkup=fn_bkup)
+            # back up files
+            if fn_bkup is not None:
+                if label is None:
+                    label = '.'
+                for fb in [f for f in os.listdir(label) if f[:3] == 'opt' and fn_bkup in f]:
+                    os.rename(f'{label}/{fb}', f'{label}/cyc{str(counter).zfill(2)}__{fb}')
+            
+        counter += 1
+
+        # REMOVE BROKEN FRAGMENTS
+        if has_fragList:
+            my_fragList = read_frag(fn=fn_frag)
+            struct = opt_atoms.copy()
+            my_fragAtoms = [struct[f] for f in my_fragList]
+            # Check connectivity
+            list_del = []
+            for i in range(len(my_fragList)):
+                if len(get_fragments(my_fragAtoms[i]))!=1:
+                    for i_del in my_fragList[i]:
+                        if i_del not in list_del:
+                            list_del.append(i_del)
+            if len(list_del) > 0:
+                print('Remove broken fragments containing:', list_del)
+                update_frag_del(list_del, fn=fn_frag)
+                del struct[list_del]
+                opt_atoms = struct
+
+        # REMOVE FRAGMENTS THAT DOES NOT BIND TO SURFACE ATOMS
+        # E.G. [SURF]-[C-O]-[C-O]
+        #      the latter CO is bonded to an adorbate but not the surface
+        if has_fragList and has_fragSurfBond:
+            my_fragList = read_frag(fn=fn_frag)
+            struct = opt_atoms.copy()
+            list1 = list(range(len(read(substrate))))
+            list_del = []
+            for i in range(len(my_fragList)):
+                if not is_bonded(struct, list1, my_fragList[i]):
+                    for i_del in my_fragList[i]:
+                        if i_del not in list_del:
+                            list_del.append(i_del)
+            if len(list_del) > 0:
+                print('Remove fragments not bonded to the surface:', list_del)
+                update_frag_del(list_del, fn=fn_frag)
+                del struct[list_del]
+                opt_atoms = struct
+
+        # REMOVE FRAGMENTS THAT FROM BOND WITH OTHER FRAGMENTS
+        if has_fragList and check_rxn_frags:
+            my_fragList = read_frag(fn=fn_frag)
+            struct = opt_atoms.copy()
+            ads_bonds = detect_bond_between_adsFrag(struct, my_fragList)
+            if len(ads_bonds) > 0:
                 list_del = []
-                for i in range(len(my_fragList)):
-                    if len(get_fragments(my_fragAtoms[i]))!=1:
-                        for i_del in my_fragList[i]:
-                            if i_del not in list_del:
-                                list_del.append(i_del)
+                for b in ads_bonds:
+                    i = random.choice(b[0:2])
+                    for i_del in my_fragList[i]:
+                        if i_del not in list_del:
+                            list_del.append(i_del)
                 if len(list_del) > 0:
-                    print('Remove broken fragments containing:', list_del)
+                    print('Remove associated fragments containing:', list_del)
                     update_frag_del(list_del, fn=fn_frag)
                     del struct[list_del]
                     opt_atoms = struct
 
-            # REMOVE FRAGMENTS THAT DOES NOT BIND TO SURFACE ATOMS
-            # E.G. [SURF]-[C-O]-[C-O]
-            #      the latter CO is bonded to an adorbate but not the surface
-            if has_fragList and has_fragSurfBond:
-                my_fragList = read_frag(fn=fn_frag)
-                struct = opt_atoms.copy()
-                list1 = list(range(len(read(substrate))))
-                list_del = []
-                for i in range(len(my_fragList)):
-                    if not is_bonded(struct, list1, my_fragList[i]):
-                        for i_del in my_fragList[i]:
-                            if i_del not in list_del:
-                                list_del.append(i_del)
-                if len(list_del) > 0:
-                    print('Remove fragments not bonded to the surface:', list_del)
+        # REMOVE FREE MOLECULES DESORBED FROM THE SURFACE
+        if chkMol:
+            geom_tmp, list_del = del_freeMol(opt_atoms, list_keep=list_keep)
+            if len(geom_tmp) < len(opt_atoms):
+                opt_atoms = geom_tmp
+                if has_fragList and len(list_del) > 0:
                     update_frag_del(list_del, fn=fn_frag)
-                    del struct[list_del]
-                    opt_atoms = struct
 
-            # REMOVE FRAGMENTS THAT FROM BOND WITH OTHER FRAGMENTS
-            if has_fragList and check_rxn_frags:
-                my_fragList = read_frag(fn=fn_frag)
-                struct = opt_atoms.copy()
-                ads_bonds = detect_bond_between_adsFrag(struct, my_fragList)
-                if len(ads_bonds) > 0:
-                    list_del = []
-                    for b in ads_bonds:
-                        i = random.choice(b[0:2])
-                        for i_del in my_fragList[i]:
-                            if i_del not in list_del:
-                                list_del.append(i_del)
-                    if len(list_del) > 0:
-                        print('Remove associated fragments containing:', list_del)
-                        update_frag_del(list_del, fn=fn_frag)
-                        del struct[list_del]
-                        opt_atoms = struct
+        # REMOVE ATOMS OUTSIDE THE SAMPLING BOX
+        if zLim is not None:
+            surf = Interface(
+                opt_atoms,
+                substrate,
+                zLim = zLim
+            )
+            if surf.has_outsideBox():
+                if has_fragList:
+                    surf.del_outsideBox_frag(fn_frag)
+                else:
+                    surf.del_outsideBox()
+                opt_atoms = surf.get_allAtoms()
 
-            # REMOVE FREE MOLECULES DESORBED FROM THE SURFACE
-            if chkMol:
-                geom_tmp, list_del = del_freeMol(opt_atoms, list_keep=list_keep)
-                if len(geom_tmp) < len(opt_atoms):
-                    opt_atoms = geom_tmp
-                    if has_fragList and len(list_del) > 0:
-                        update_frag_del(list_del, fn=fn_frag)
+        # REMOVE ATOMS THAT DO NOT FORM SPECIFIED BONDS
+        if len(rmAtomsNotInBond) > 0:
+            # e.g. if [['H', 'Pt']]
+            # 'H' will be removed if not in Pt-H
+            struct = opt_atoms.copy()
+            list_del = []
+            for p in rmAtomsNotInBond:
+                list_a0 = [a.index for a in struct if a.symbol==p[0]]
+                list_a1 = [a.index for a in struct if a.symbol==p[1]]
+                for a0 in list_a0:
+                    if not is_bonded(struct, [a0], list_a1):
+                        list_del.append(a0)
+            if len(list_del) > 0:
+                print(f'Atoms {list_del} are removed becasue not in required bonds.')
+                del struct[list_del]
+                opt_atoms = struct
 
-            # REMOVE ATOMS OUTSIDE THE SAMPLING BOX
-            if zLim is not None:
-                surf = Interface(
-                    opt_atoms,
-                    substrate,
-                    zLim = zLim
-                )
-                if surf.has_outsideBox():
-                    if has_fragList:
-                        surf.del_outsideBox_frag(fn_frag)
-                    else:
-                        surf.del_outsideBox()
-                    opt_atoms = surf.get_allAtoms()
+        # REMOVE BROKEN FRAGMENTS
+        if has_fragList:
+            my_fragList = read_frag(fn=fn_frag)
+            struct = opt_atoms.copy()
+            my_fragAtoms = [struct[f] for f in my_fragList]
+            # Check connectivity
+            list_del = []
+            for i in range(len(my_fragList)):
+                if len(get_fragments(my_fragAtoms[i]))!=1:
+                    for i_del in my_fragList[i]:
+                        if i_del not in list_del:
+                            list_del.append(i_del)
+            if len(list_del) > 0:
+                print('Remove broken fragments containing:', list_del)
+                update_frag_del(list_del, fn=fn_frag)
+                del struct[list_del]
+                opt_atoms = struct
 
-            # REMOVE ATOMS THAT DO NOT FORM SPECIFIED BONDS
-            if len(rmAtomsNotInBond) > 0:
-                # e.g. if [['H', 'Pt']]
-                # 'H' will be removed if not in Pt-H
-                struct = opt_atoms.copy()
-                list_del = []
-                for p in rmAtomsNotInBond:
-                    list_a0 = [a.index for a in struct if a.symbol==p[0]]
-                    list_a1 = [a.index for a in struct if a.symbol==p[1]]
-                    for a0 in list_a0:
-                        if not is_bonded(struct, [a0], list_a1):
-                            list_del.append(a0)
-                if len(list_del) > 0:
-                    print(f'Atoms {list_del} are removed becasue not in required bonds.')
-                    del struct[list_del]
-                    opt_atoms = struct
-
-            # REMOVE BROKEN FRAGMENTS
-            if has_fragList:
-                my_fragList = read_frag(fn=fn_frag)
-                struct = opt_atoms.copy()
-                my_fragAtoms = [struct[f] for f in my_fragList]
-                # Check connectivity
-                list_del = []
-                for i in range(len(my_fragList)):
-                    if len(get_fragments(my_fragAtoms[i]))!=1:
-                        for i_del in my_fragList[i]:
-                            if i_del not in list_del:
-                                list_del.append(i_del)
-                if len(list_del) > 0:
-                    print('Remove broken fragments containing:', list_del)
-                    update_frag_del(list_del, fn=fn_frag)
-                    del struct[list_del]
-                    opt_atoms = struct
-
-            natoms_removed = len(my_atoms) - len(opt_atoms)
-            my_atoms = opt_atoms
-            if natoms_removed > 0:
-                print(f'Redo the last opt step due to removal of {natoms_removed} atoms')
-                continue
-            elif natoms_removed == 0:
-                print('The geometry is good! CONVERGED!')
-                continueRunning = False
+        natoms_removed = len(my_atoms) - len(opt_atoms)
+        my_atoms = opt_atoms
+        if natoms_removed > 0:
+            print(f'Redo the last opt step due to removal of {natoms_removed} atoms')
+            continue
+        elif natoms_removed == 0:
+            print('The geometry is good! CONVERGED!')
+            continueRunning = False
+            break
     return my_atoms
