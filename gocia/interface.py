5f07404e506df45b669d89984d6c832423774a5f
jerbez <erbezjake@gmail.com>
2025-07-10 15:31:32 -0700

    Update interface.py

--- gocia/interface.py
+++ gocia/interface.py
@@ -878,7 +878,7 @@ class Interface:
         )
         self.set_allAtoms(tmpInterfc.get_allAtoms())
 
-    def growMut_frag(self, fragPool, bondRejList=None, growProb):
+    def growMut_frag(self, fragPool, bondRejList=None, growProb=None):
         print(' |- Growth mutation:', end = '\t')
         from gocia.geom.build import grow_frag
         tmpInterfc = self.copy()
